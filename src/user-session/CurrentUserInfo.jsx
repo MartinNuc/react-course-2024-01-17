fc15d1b80bc3678974d4f42ca8938a8e8884acba
MartinNuc <martin.nuc@productboard.com>
2024-01-19 13:52:21 +0100

    useReducer

--- src/user-session/CurrentUserInfo.jsx
+++ src/user-session/CurrentUserInfo.jsx
@@ -1,10 +1,10 @@
 import { useUser } from "./UserContext"
 
 export function CurrentUserInfo() {
-  const {user, logout} = useUser();
+  const {user, timeLeft, logout} = useUser();
 
   return <div data-testid="currentUserInfo">
-    Logged in as {user.username}
+    Logged in as {user.username} (expires in {timeLeft}s)
     <button onClick={logout}>Logout</button>
   </div>
 }
--- src/user-session/UserContext.js
+++ src/user-session/UserContext.js
@@ -1,14 +1,61 @@
-import React, { useContext,useState } from 'react';
+import React, { useContext,useEffect,useReducer } from 'react';
 
 const UserContext = React.createContext();
 
+const initialState = {
+  user: null,
+  timeLeft: 0
+};
+
+const reducer = (state, action) => {
+  console.log(action, state)
+  switch(action.type) {
+    case 'login':
+      return {
+        ...state,
+        user: action.payload,
+        timeLeft: 10
+      }
+    case 'logout':
+      return {
+        ...state,
+        user: null
+      }
+    case 'sessionTick':
+      if (state.timeLeft <= 1) {
+        return {
+          ...state,
+          user: null
+        }
+      }
+      return {
+        ...state,
+        timeLeft: state.timeLeft - 1
+      }
+    default:
+      throw new Error(`Unknown action ${action.type}`);
+  }
+}
+
 export function UserContextProvider({children}) {
-  const [user, setUser] = useState(null);
+  const [session, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    if (!session.user) { return; }
+
+    const intervalRef = setInterval(() => dispatch({type: 'sessionTick'}), 1000);
+
+    return () => {
+      if (!session.user) { return; }
+      clearInterval(intervalRef)
+    }
+  }, [session.user]);
 
   return <UserContext.Provider value={{
-    login: (user) => setUser(user),
-    logout: () => setUser(null),
-    user
+    login: (user) => dispatch({ type: 'login', payload: user}),
+    logout: () => dispatch({ type: 'logout' }),
+    user: session.user,
+    timeLeft: session.timeLeft
   }}>
     {children}
   </UserContext.Provider>
